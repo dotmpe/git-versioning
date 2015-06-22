a224a849af32e7955b53debdcc142c895d50997c
B. van Berkum <dev@dotmpe.com>
2015-06-22 02:30:26 +0200

    Added TODO note (git-versioning/0.0.27-master)

--- tools/cmd/test-for-clean-branch.sh
+++ tools/cmd/test-for-clean-branch.sh
@@ -2,3 +2,6 @@
 # Id: git-versioning/0.0.27-master tools/cmd/test-for-clean-branch.sh
 # 1:branchref 2:branchcommitsha1
 
+# TODO: test wether V_MAIN_DOC version for pre-release tag
+# must match branch name
+
