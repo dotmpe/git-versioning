58bfef83c3099c1a7d9e676c9434d55c7dc0759b
B. van Berkum <dev@dotmpe.com>
2015-08-23 19:10:36 +0200

    Setting prod. paths (git-versioning/0.0.28-dev+20150823-1641)

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env bash
 
-test -n "$SRC_PREFIX" || SRC_PREFIX=$HOME
+test -n "$SRC_PREFIX" || {
+SRC_PREFIX=$HOME
+}
 
-# Check for BATS shell test runner or install
-test -x "$(which bats)" && {
-  bats --version
-} || {
+test -n "$PREFIX" || {
+PREFIX=/usr/local
+}
+
+test -n "$SRC_PREFIX" || {
+  echo "Not sure where checkout"
+  exit 1
+}
+
+test -n "$PREFIX" || {
+  echo "Not sure where to install"
+  exit 1
+}
+
+test -d $SRC_PREFIX || mkdir -vp $SRC_PREFIX
+test -d $PREFIX || mkdir -vp $PREFIX
+
+
+install_bats()
+{
   echo "Installing bats"
   pushd $SRC_PREFIX
   git clone https://github.com/sstephenson/bats.git
   cd bats
-  sudo ./install.sh /usr/local
+  ./install.sh $PREFIX
   popd
-  bats --version
 }
+
+# Check for BATS shell test runner or install
+test -x "$(which bats)" || {
+  install_bats
+  export PATH=$PATH:$PREFIX/bin
+}
+
+bats --version
 
 # Id: git-versioning/0.0.27-test install-dependencies.sh
