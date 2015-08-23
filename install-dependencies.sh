188d40427f36c9052ff636db903e6f445144beed
B. van Berkum <dev@dotmpe.com>
2015-08-23 16:19:07 +0200

    Updated install-dep script a bit

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+test -n "$PREFIX" || PREFIX=$HOME
+
 # Check for BATS shell test runner or install
 test -x "$(which bats)" && {
   bats --version
 } || {
   echo "Installing bats"
-  pushd $HOME
+  pushd $PREFIX
   git clone https://github.com/sstephenson/bats.git
   cd bats
   sudo ./install.sh /usr/local
