a1acd8704bfd228e5509e46e171d36ec8cd6beee
B. van Berkum <dev@dotmpe.com>
2016-04-10 05:03:29 +0200

    Upgraded install-deps

---
+++ .package.sh
@@ -0,0 +1,6 @@
+
+package_pd_meta_check="vchk bats-specs"
+package_pd_meta_test="vchk bats-specs bats"
+package_pd_meta_git_hooks_pre_commit=./script/scm/pre-commit.sh
+package_pd_meta_init="./install-dependencies.sh git"
+
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,28 +1,61 @@
 #!/usr/bin/env bash
 
+set -e
+
+test -z "$Build_Debug" || set -x
+
 test -n "$sudo" || sudo=
 
-test -n "$SRC_PREFIX" || {
-SRC_PREFIX=/src/
+test -n "$GIT_HOOK_NAMES" || GIT_HOOK_NAMES="apply-patch commit-msg post-update pre-applypatch pre-commit pre-push pre-rebase prepare-commit-msg update"
+
+
+generate_git_hooks()
+{
+  # Create default script from pd-check
+  test -n "$package_pd_meta_git_hooks_pre_commit_script" || {
+    package_pd_meta_git_hooks_pre_commit_script="pd check $package_pd_meta_check"
+  }
+
+	for script in $GIT_HOOK_NAMES
+	do
+		t=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_'))
+		test -n "$t" || continue
+    test -e "$t" || {
+      s=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_')_script)
+      test -n "$s" || {
+        echo "No default git $script script. "
+        return
+      }
+
+      mkdir -vp $(dirname $t)
+      echo "$s" >$t
+      chmod +x $t
+      echo "Installed $script GIT commit hook"
+    }
+  done
 }
 
-test -n "$PREFIX" || {
-PREFIX=/usr/local
+install_git_hooks()
+{
+	for script in $GIT_HOOK_NAMES
+	do
+		t=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_'))
+		test -n "$t" || continue
+		l=.git/hooks/$script
+		test ! -e "$l" || {
+			test -h $l && {
+				test "$(readlink $l)" = "../../$t" && continue || {
+					rm $l
+				}
+			} ||	{
+				echo "Git hook exists and is not a symlink: $l"
+				continue
+			}
+		}
+		( cd .git/hooks; ln -s ../../$t $script )
+		echo "Symlinked GIT hook to script: $script -> $t"
+	done
 }
-
-test -n "$SRC_PREFIX" || {
-  echo "Not sure where checkout"
-  exit 1
-}
-
-test -n "$PREFIX" || {
-  echo "Not sure where to install"
-  exit 1
-}
-
-test -d $SRC_PREFIX || ${sudo} mkdir -vp $SRC_PREFIX
-test -d $PREFIX || ${sudo} mkdir -vp $PREFIX
-
 
 install_bats()
 {
@@ -34,12 +67,52 @@ install_bats()
   popd
 }
 
-# Check for BATS shell test runner or install
-test -x "$(which bats)" || {
-  install_bats
+
+main_entry()
+{
+  test -n "$1" || set -- '*'
+
+  case "$1" in build )
+			test -n "$SRC_PREFIX" || {
+				echo "Not sure where checkout"
+				exit 1
+			}
+
+			test -n "$PREFIX" || {
+				echo "Not sure where to install"
+				exit 1
+			}
+
+			test -d $SRC_PREFIX || ${sudo} mkdir -vp $SRC_PREFIX
+			test -d $PREFIX || ${sudo} mkdir -vp $PREFIX
+		;;
+	esac
+
+  case "$1" in '*'|project|git )
+      git --version >/dev/null || { echo "Sorry, GIT is a pre-requisite"; exit 1; }
+			test -x $(which jsotk.py 2>/dev/null) || {
+				test -e .package.sh || {
+					echo "Sorry, jsotk.py is a pre-requisite"; exit 1;
+				}
+			}
+			test -e .package.sh || jsotk.py yaml2sh .package.yaml [id=invidia]
+			source .package.sh
+    ;; esac
+
+  case "$1" in '*'|project|git )
+  		generate_git_hooks || return $?
+  		install_git_hooks || return $?
+    ;; esac
+
+  case "$1" in '*'|build|test|sh-test|bats )
+      test -x "$(which bats)" || install_bats || return $?
+    ;; esac
+
+  echo "OK. All pre-requisites for '$1' checked"
 }
 
-
-bats --version
+test "$(basename $0)" = "install-dependencies.sh" && {
+  main_entry $@ || exit $?
+}
 
 # Id: git-versioning/0.0.27-test install-dependencies.sh
