2417b660ea0727b30b405a94365570e6ff0fd817
B. van Berkum <berend@brixcrm.nl>
2016-04-12 15:45:51 +0200

    Trying to fix production build

--- ci-build.sh
+++ ci-build.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+version=0.0.31-dev+20160412-1532 # git-versioning
+
 case "$ENV" in
 
    production )
-      ./configure.sh /usr/local && sudo ENV=$ENV ./install.sh && make test build
-      DESCRIBE="$(git describe --tags)"
+      ./configure.sh /usr/local && sudo ENV=$ENV ./install.sh && make test
+      # strip meta tags
+      DESCRIBE="$(echo "$version" | awk -F+ '{print $1}')"
       grep '^'$DESCRIBE'$' ChangeLog.rst && {
-        echo "TODO: get log, tag"
-        exit 1
+        make build
       } || {
         echo "Not a release: missing change-log entry $DESCRIBE: grep $DESCRIBE ChangeLog.rst)"
+        echo "Not building package"
       }
     ;;
+
    test* | dev* )
      ./configure.sh && make build test ;;
    * )
