91d2b0539339a621a391fc9d1240000bcc3bc9f8
B. van Berkum <dev@dotmpe.com>
2016-05-05 23:47:52 +0200

    Updated package metadata

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,56 +6,7 @@ test -z "$Build_Debug" || set -x
 
 test -n "$sudo" || sudo=
 
-test -n "$GIT_HOOK_NAMES" || GIT_HOOK_NAMES="apply-patch commit-msg post-update pre-applypatch pre-commit pre-push pre-rebase prepare-commit-msg update"
 
-
-generate_git_hooks()
-{
-  # Create default script from pd-check
-  test -n "$package_pd_meta_git_hooks_pre_commit_script" || {
-    package_pd_meta_git_hooks_pre_commit_script="pd check $package_pd_meta_check"
-  }
-
-	for script in $GIT_HOOK_NAMES
-	do
-		t=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_'))
-		test -n "$t" || continue
-    test -e "$t" || {
-      s=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_')_script)
-      test -n "$s" || {
-        echo "No default git $script script. "
-        return
-      }
-
-      mkdir -vp $(dirname $t)
-      echo "$s" >$t
-      chmod +x $t
-      echo "Installed $script GIT commit hook"
-    }
-  done
-}
-
-install_git_hooks()
-{
-	for script in $GIT_HOOK_NAMES
-	do
-		t=$(eval echo \$package_pd_meta_git_hooks_$(echo $script|tr '-' '_'))
-		test -n "$t" || continue
-		l=.git/hooks/$script
-		test ! -e "$l" || {
-			test -h $l && {
-				test "$(readlink $l)" = "../../$t" && continue || {
-					rm $l
-				}
-			} ||	{
-				echo "Git hook exists and is not a symlink: $l"
-				continue
-			}
-		}
-		( cd .git/hooks; ln -s ../../$t $script )
-		echo "Symlinked GIT hook to script: $script -> $t"
-	done
-}
 
 install_bats()
 {
@@ -97,10 +48,10 @@ main_entry()
 	esac
 
   case "$1" in '*'|project|git )
-      git --version >/dev/null || { echo "Sorry, GIT is a pre-requisite"; exit 1; }
+      git --version >/dev/null || { echo "GIT required"; exit 1; }
 			test -x $(which jsotk.py 2>/dev/null) || {
 				test -e .package.sh || {
-					echo "Sorry, jsotk.py is a pre-requisite"; exit 1;
+          echo "jsotk.py (yaml2sh) required"; exit 1;
 				}
 			}
 			test -e .package.sh || jsotk.py yaml2sh .package.yaml [id=invidia]
@@ -108,8 +59,8 @@ main_entry()
     ;; esac
 
   case "$1" in '*'|project|git )
-  		generate_git_hooks || return $?
-  		install_git_hooks || return $?
+      test -x "$(which pd)" || { echo "Pd (projectdir.sh) required"; exit 1; }
+  		pd update . || return $?
     ;; esac
 
   case "$1" in '*'|build|test|sh-test|bats )
---
+++ script/scm/pre-commit.sh
@@ -0,0 +1,1 @@
+pd check vchk bats-specs
