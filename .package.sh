ae681fe5806922caad25438bd35ca0bd0c6b485e
B. van Berkum <dev@dotmpe.com>
2016-05-05 23:07:59 +0200

    Updated package meta format

--- .package.sh
+++ .package.sh
@@ -1,6 +1,9 @@
-
-package_pd_meta_check="vchk bats-specs"
+package_environments__0=development
+package_id=git-versioning
+package_version=0.1.0
+package_main=git-versioning
+package_type=application/x-project-mpe
 package_pd_meta_test="vchk bats-specs bats"
 package_pd_meta_git_hooks_pre_commit=./script/scm/pre-commit.sh
 package_pd_meta_init="./install-dependencies.sh git"
-
+package_pd_meta_check="vchk bats-specs"
